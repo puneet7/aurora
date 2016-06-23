ffb9b5277476a2884ac45559d5be3f57b4371153
Abhishek Juneja <abhishek.juneja145@gmail.com>
2016-06-23 14:47:06 +0530

    added the ajax backend code for populating the wiki module

--- learn/views.py
+++ learn/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from pprint import pprint
+from django.views.decorators.csrf import csrf_exempt
 
 #ideal-guacamole imports
-from learn.utils import get_all_domains, get_all_technologies, get_resources_grouped_by_tech, get_resources_grouped_by_domain, get_domains_and_slugs, get_tech_and_slugs
+from learn.utils import get_all_domains, get_all_technologies, get_resources_grouped_by_tech, get_resources_grouped_by_domain, get_domains_and_slugs, get_tech_and_slugs, get_modal_data
 # Create your views here.
 
 def hello(request):
@@ -49,5 +50,12 @@ def download_all_domain_data_view(request):
 def download_all_tech_data_view(request):
     pass
 
-def get_wiki_view(request):
-    pass
+
+@csrf_exempt
+def wiki_term_data(request):
+    if request.method == 'POST' and request.is_ajax():
+        json_string = request.body.decode(encoding='UTF-8')
+        data = json.loads(json_string)
+        wiki_term =  str(data['wiki_term'])
+        wiki_data = get_modal_data(wiki_term)
+    return HttpResponse(json.dumps(True))
